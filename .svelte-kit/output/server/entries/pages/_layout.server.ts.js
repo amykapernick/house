9bb413ef93c1e6f4a5214e6bf99d97295c982433
Amy Kapernick <amy@kapers.dev>
2026-06-26 14:18:20 +0000

    updated tracking

--- .svelte-kit/output/server/entries/pages/_layout.server.ts.js
+++
@@ -1,7 +0,0 @@
-import { t as private_env } from "../../chunks/shared-server.js";
-//#region src/routes/+layout.server.ts
-var load = async () => {
-	return { clerkPublishableKey: private_env.CLERK_PUBLISHABLE_KEY ?? "" };
-};
-//#endregion
-export { load };
